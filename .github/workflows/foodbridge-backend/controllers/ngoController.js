e43228d4f33a14b6c6c992abe0c7d9a8ab900073
Krish Ramoju <71651407+Krishramoju@users.noreply.github.com>
2025-04-18 18:28:58 +0530

    Update ngoController.js

--- .github/workflows/foodbridge-backend/controllers/ngoController.js
+++ .github/workflows/foodbridge-backend/controllers/ngoController.js
@@ -1,1 +1,22 @@
 
+const NGO = require("../models/NGO");
+
+exports.createNGO = async (req, res) => {
+  const { name, location, contactInfo } = req.body;
+  try {
+    const ngo = new NGO({ name, location, contactInfo });
+    await ngo.save();
+    res.status(201).json(ngo);
+  } catch (err) {
+    res.status(400).json({ message: "Error creating NGO" });
+  }
+};
+
+exports.getNGOs = async (req, res) => {
+  try {
+    const ngos = await NGO.find();
+    res.status(200).json(ngos);
+  } catch (err) {
+    res.status(400).json({ message: "Error fetching NGOs" });
+  }
+};
