b3b143acd472b0edae40f77fd6ba9cf16ed36a13
Krish Ramoju <71651407+Krishramoju@users.noreply.github.com>
2025-04-18 18:29:28 +0530

    Update userController.js

--- .github/workflows/foodbridge-backend/controllers/userController.js
+++ .github/workflows/foodbridge-backend/controllers/userController.js
@@ -1,1 +1,26 @@
 
+const User = require("../models/User");
+
+exports.registerUser = async (req, res) => {
+  const { username, password, role } = req.body;
+  try {
+    const user = new User({ username, password, role });
+    await user.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ message: "Error registering user" });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user || user.password !== password) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    res.status(200).json({ message: "Login successful", user });
+  } catch (err) {
+    res.status(400).json({ message: "Error logging in" });
+  }
+};
