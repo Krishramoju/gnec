09f7e623584eae148938c79f54c3d1f4abc954f2
Krish Ramoju <71651407+Krishramoju@users.noreply.github.com>
2025-04-18 18:27:30 +0530

    Update NGO.js

--- .github/workflows/foodbridge-backend/models/NGO.js
+++ .github/workflows/foodbridge-backend/models/NGO.js
@@ -1,1 +1,11 @@
 
+const mongoose = require("mongoose");
+
+const ngoSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  location: { type: String, required: true },
+  contactInfo: { type: String, required: true },
+  foodReceived: [{ type: mongoose.Schema.Types.ObjectId, ref: "Food" }],
+});
+
+module.exports = mongoose.model("NGO", ngoSchema);
