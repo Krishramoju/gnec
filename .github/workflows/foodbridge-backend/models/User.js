556c0f324e0ac8cbcb739ca4821e7b6abea74c4d
Krish Ramoju <71651407+Krishramoju@users.noreply.github.com>
2025-04-18 18:27:46 +0530

    Update User.js

--- .github/workflows/foodbridge-backend/models/User.js
+++ .github/workflows/foodbridge-backend/models/User.js
@@ -1,1 +1,10 @@
 
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true, enum: ["donor", "ngo"], default: "donor" },
+});
+
+module.exports = mongoose.model("User", userSchema);
