b3734ff05bc10f9859a3c2b8f89b4a4140e08992
Krish Ramoju <71651407+Krishramoju@users.noreply.github.com>
2025-04-18 18:38:14 +0530

    Update db.js

--- .github/workflows/foodbridge-backend/config/db.js
+++ .github/workflows/foodbridge-backend/config/db.js
@@ -1,1 +1,6 @@
 
+module.exports = {
+  mongoURI: process.env.MONGO_URI || "mongodb://localhost:27017/food-donation",
+  jwtSecret: process.env.JWT_SECRET || "default_jwt_secret",
+  port: process.env.PORT || 5000,
+};
