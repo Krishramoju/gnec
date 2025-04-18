dbc913e8e2ce6fd5a0c6125529f5e391740e737c
Krish Ramoju <71651407+Krishramoju@users.noreply.github.com>
2025-04-18 18:24:27 +0530

    Update App.js

--- .github/workflows/foodbridge-frontend/src/App.js
+++ .github/workflows/foodbridge-frontend/src/App.js
@@ -1,1 +1,16 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
 
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
